65132989095ded647549cd3d873fa3970fa92b9c
Saikiran <duddukurisaikiran.com>
2023-01-05 02:04:16 +0530

    v10 to v11 updated

--- src/app/components/portfilio/portfilio.component.spec.ts
+++ src/app/components/portfilio/portfilio.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PortfilioComponent } from './portfilio.component';
 
@@ -6,7 +6,7 @@ describe('PortfilioComponent', () => {
   let component: PortfilioComponent;
   let fixture: ComponentFixture<PortfilioComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PortfilioComponent ]
     })
